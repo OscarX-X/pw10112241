d4ffaff95e8d42ea715092ac50e7aaa82cc63bf9
Oscar Beltrán Heras <132297762+OscarX-X@users.noreply.github.com>
2024-04-25 10:52:27 -0700

    Conexion MySQL

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,9 +6,38 @@ let app = express(); // recibe al servidor y da una respuesta a la conexion
 app.use(express.json()); 
 app.use(cors());
 
+// Conexion a MySQL
+let conexion = mysql.createConnection ( {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+})
+
+// Nos conectamos a MySQL 
+conexion.connect(function(error) {
+    if(error) {
+        throw error;
+    } else {
+        console.log('Conectado a la base de datos');
+    }
+})
+
+// Seleccionamos todos lo clientes
+app.get('/api/clientes2', (req, res) => {
+    conexion.query('SELECT * FROM clientes2', (error, filas) => {
+        if (error) {
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    });
+});
+
 // Rutas de acceso
 app.get("/", function(req, res) {
-    res.send("Rutassssssss de iniciosssssss")
+    res.send("Ruta de inicio")
 })
 
 // Encender el servidor
